ea0ba89d0c2ec9a6b1735851cee86267bae07036
Sophia Guilbeault <sophiaguilbeault@crc-dot1x-nat-10-239-91-148.bu.edu>
2024-10-31 11:01:27 -0400

    docstring for random_utils

--- HW4_docstrings/meal_max/meal_max/utils/random_utils.py
+++ HW4_docstrings/meal_max/meal_max/utils/random_utils.py
@@ -8,6 +8,16 @@ configure_logger(logger)
 
 
 def get_random() -> float:
+    """
+     Fetches a random float with two decimal places starting from 0 in the catalog from random.org.
+
+     Returns:
+        float: The random number fetched from random.org.
+
+     Raises:
+        RuntimeError: If the request to random.org fails or returns an invalid response.
+        ValueError: If the response from random.org is not a valid float.
+    """
     url = "https://www.random.org/decimal-fractions/?num=1&dec=2&col=1&format=plain&rnd=new"
 
     try:
