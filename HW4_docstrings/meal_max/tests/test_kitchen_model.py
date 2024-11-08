4ff106362877ba5ba591f0654f361cc0268c3983
AngelManson <157187860+AngelManson@users.noreply.github.com>
2024-11-08 14:54:46 -0500

    smh

--- HW4_docstrings/meal_max/tests/test_kitchen_model.py
+++ HW4_docstrings/meal_max/tests/test_kitchen_model.py
@@ -35,7 +35,7 @@ def mock_cursor(mocker):
     def mock_get_db_connection():
         yield mock_conn  # Yield the mocked connection object
 
-    mocker.patch("meal_max.meal_max.models.kitchen_model.get_db_connection", mock_get_db_connection)
+    mocker.patch("meal_max.models.kitchen_model.get_db_connection", mock_get_db_connection)
 
     return mock_cursor  # Return the mock cursor so we can set expectations per test
 
