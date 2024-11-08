531935e200e71c0b616c008b3f748e4321566eb1
Sophia Guilbeault <sophiaguilbeault@crc-dot1x-nat-10-239-91-148.bu.edu>
2024-11-07 20:41:47 -0500

    still trying

--- HW4_docstrings/meal_max/smoketest.sh
+++ HW4_docstrings/meal_max/smoketest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the base URL for the Flask API
-BASE_URL="http://localhost:5003/api"
+BASE_URL="http://172.17.0.2:5000/api"
 
 # Flag to control whether to echo JSON output
 ECHO_JSON=false
